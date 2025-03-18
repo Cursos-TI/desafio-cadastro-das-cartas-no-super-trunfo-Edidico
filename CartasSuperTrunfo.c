f1185e6efe4d020663761a906399352a11c4c174
Edidico <edidico@gmail.com>
2025-03-18 20:07:56 +0000

    Finalizado!

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -24,8 +24,19 @@ printf("Código: %s\n", codigo1);
 printf("Nome da Cidade: %s\n", nomeCidade1);
 printf("População: %d\n", populacao1);
 printf("Área: %.2f km²\n", area1);
-printf("PIB: %.2f bilhões de euros\n", pib1);
+printf("PIB: %.2f bilhões de reais\n", pib1);
 printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
+
+printf("Carta 2:\n");
+printf("Estado: %c\n", estado2);
+printf("Código: %s\n", codigo2);
+printf("Nome da Cidade: %s\n", nomeCidade2);
+printf("População: %d\n", populacao2);
+printf("Área: %.2f km²\n", area2);
+printf("PIB: %.2f bilhões de reais\n", pib2);
+printf("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
+
+return 0;
 
 
 
