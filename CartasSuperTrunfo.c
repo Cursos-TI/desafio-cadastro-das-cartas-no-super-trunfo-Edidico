d2f5c8c180c5043d87df0a73f376304c4c4cacb1
Edidico <edidico@gmail.com>
2025-03-18 20:01:34 +0000

    Exibição dos dados da primeira carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,10 +7,18 @@ char codigo1[] = "B01";
 char nomeCidade1[] = "Brasilia";
 int populacao1 = 1190000;
 float area1 = 161.38;
-float pib1 = 543;
+float pib1 = 542;
 int pontosTuristicos1 = 28;
 
 char estado2 = 'A';
+char codigo2[] = "A02";
+char nomeCidade2[] = "Amazonas";
+int populacao2 = 3120000;
+float area2 = 202.0;
+float pib2 = 646.3;
+int pontosTuristicos2 = 300;
+
+
 
 
 
