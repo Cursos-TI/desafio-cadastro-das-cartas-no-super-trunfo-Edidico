6a54f7564d13f6dea5aa1f50f7df29c6ef5e5adf
Edidico <edidico@gmail.com>
2025-03-18 20:04:14 +0000

    Exibição dos dados da segunda carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,7 +18,14 @@ float area2 = 202.0;
 float pib2 = 646.3;
 int pontosTuristicos2 = 300;
 
-
+printf("Carta 1:\n");
+printf("Estado: %c\n", estado1);
+printf("Código: %s\n", codigo1);
+printf("Nome da Cidade: %s\n", nomeCidade1);
+printf("População: %d\n", populacao1);
+printf("Área: %.2f km²\n", area1);
+printf("PIB: %.2f bilhões de euros\n", pib1);
+printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
 
 
 
