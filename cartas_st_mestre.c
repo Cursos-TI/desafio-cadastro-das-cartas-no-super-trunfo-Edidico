a785194c5394fecbbf8221e60f0a282bac51124a
Edidico <edidico@gmail.com>
2025-03-24 23:29:58 +0000

    // variaveis

---
+++ cartas_st_mestre.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (){
+    
+    // Váriaveis
+    char estado1, estado2;
+    char cidade1[100], cidade2[100];
+    char codigo1[4], codigo2[4];
+    int população1, população2;
+    int pontosturisticos1, pontosturisticos2;
+    float area1, area2;
+    float pib1, pib2;
+}
