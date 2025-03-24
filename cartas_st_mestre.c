a136133686c6062e6d8922cd1940fdb9794e80a0
Edidico <edidico@gmail.com>
2025-03-24 23:34:59 +0000

    // Entrada de dados da primeira carta

--- cartas_st_mestre.c
+++ cartas_st_mestre.c
@@ -13,4 +13,18 @@ int main (){
 
     printf("[SEJA BEM-VINDO AO SUPER TRUNFO]\n\n");
 
+    printf("Exemplo de uma carta:\n\n");
+
+    printf("Estado (A-H): A\n");
+    printf("Código da carta (01-04): A01\n");
+    printf("Nome da cidade (Sem caracteres especiais): Sao Paulo\n");
+    printf("População (Dentro dos números naturais): 12320000\n");
+    printf("Área(em km²): 1511.11\n");
+    printf("Densidade demográfica: 8152.94\n");
+    printf("PIB (em bilhões): 699.28\n");
+    printf("PIB per capita: 56759.74\n");
+    printf("Número de pontos turísticos: 50\n");
+
+    
+
 }
